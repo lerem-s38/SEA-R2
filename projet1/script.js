0887c0d11808b45bfa07c2a3b00ccd9b8d6f2898
Rom-mtl <romain.montel@gmail.com>
2020-10-01 15:46:53 +0200

    Formulaire OK

--- projet1/script.js
+++ projet1/script.js
@@ -1,19 +1,45 @@
 console.log('HELLO WORLD');
 
-// Animation Menu-Burger
-const burger = document.querySelector('.burger');
-
-burger.addEventListener('click', () => {
-  burger.classList.toggle('active');
-});
+// ------------------------------------------------------------------------------
+// ------------------------------------------------------------------------------
 
 // AJOUT / DISPARITION DU FORMULAIRE
 
-const showForm = document.querySelector('#btnAddContenu');
+// JAVASCRIPT BOUTTONS
+// EVENT BOUTTON ADD
+let showForm = document.querySelector('.btnAddContenu1');
 showForm.addEventListener('click', showHide);
 
+// EVENT BOUTTON SUPP
+
+let hideForm = document.querySelector('.closeSubmit1');
+hideForm.addEventListener('click', hideShow);
+
+// HTML BOUTTONS
+// EVENT BOUTTON ADD
+showForm = document.querySelector('.btnAddContenu2');
+showForm.addEventListener('click', showHide);
+
+// EVENT BOUTTON SUPP
+
+hideForm = document.querySelector('.closeSubmit2');
+hideForm.addEventListener('click', hideShow);
+
+// GIT BOUTTONS
+// EVENT BOUTTON ADD
+showForm = document.querySelector('.btnAddContenu3');
+showForm.addEventListener('click', showHide);
+
+// EVENT BOUTTON SUPP
+
+hideForm = document.querySelector('.closeSubmit3');
+hideForm.addEventListener('click', hideShow);
+// ------------------------FUNCTION AFFICHE CACHE LE FORMULAIRE-----------------------------------------
+
 function showHide(event) {
+  console.log('test');
   let statut = event.currentTarget.parentNode;
+
   const form = document.createElement('form');
   form.classList.add('formInstance');
   statut.appendChild(form);
@@ -24,7 +50,7 @@ function showHide(event) {
   form.appendChild(labelForSubject);
 
   const select = document.createElement('select');
-  select.classList.add('formInstance');
+  select.classList.add('selectInstance');
   form.appendChild(select);
 
   const array = [
@@ -42,7 +68,7 @@ function showHide(event) {
     select.appendChild(option);
   }
 
-  const labelForName = document.createElement('labelName');
+  const labelForName = document.createElement('label');
   labelForName.classList.add('labelName');
   labelForName.innerText = 'Entrer votre prénom:';
   form.appendChild(labelForName);
@@ -51,47 +77,53 @@ function showHide(event) {
   inputText.classList.add('inputName');
   inputText.type = 'text';
   inputText.name = 'input_name';
-  inputText.innerText = ' Votre nom ici';
   form.appendChild(inputText);
+
+  const labelForMsg = document.createElement('label');
+  labelForMsg.classList.add('labelMsg');
+  labelForMsg.innerText = 'Entrer votre message:';
+  form.appendChild(labelForMsg);
+
+  const inputMsg = document.createElement('textarea');
+  inputMsg.classList.add('textarea');
+  inputMsg.name = 'input_msg';
+  form.appendChild(inputMsg);
+
+  const subBtn = document.createElement('button');
+  subBtn.classList.add('subBtn');
+  subBtn.innerText = 'ENVOYER';
+  form.appendChild(subBtn);
+
+  let subMsg = document.querySelector('.subBtn');
+  subMsg.addEventListener('click', submitMsg);
+
+  function submitMsg(event) {
+    event.preventDefault();
+    let divCible = event.currentTarget.parentNode.parentNode.parentNode;
+    console.log(divCible);
+
+    let newDivCibleContenu = document.createElement('p');
+    divCible.insertBefore(newDivCibleContenu, divCible.firstChild);
+    let textWilder = document.querySelector('.textarea').value;
+    newDivCibleContenu.innerText = textWilder;
+
+    let newDivCibleHun = document.createElement('p');
+    divCible.insertBefore(newDivCibleHun, divCible.firstChild);
+    let subjectWilder = document.querySelector('.selectInstance').value;
+    newDivCibleHun.innerText = subjectWilder;
+
+    let newDivCibleHdeux = document.createElement('p');
+    divCible.insertBefore(newDivCibleHdeux, divCible.firstChild);
+    let nameWilder = document.querySelector('.inputName').value;
+    newDivCibleHdeux.innerText = nameWilder;
+  }
 }
 
-/* <form id='formIdea'>
-  <label for='sujet-select'>Choisissez un sujet:</label>
+function hideShow(event) {
+  let statut2 = event.currentTarget.parentNode.children[2].remove();
+}
 
-  <select name='sujets' id='sujet-select'>
-    <option value=''>Liste des sujets</option>
-    <option value='astuce'>Astuce</option>
-    <option value='code'>Code brut</option>
-    <option value='bonne_pratique'>Bonne pratique</option>
-    <option value='Bug'>Signaler un bug</option>
-  </select>
+// ------------------------------------------------------------------------------
+// ------------------------------------------------------------------------------
 
-  <div>
-    <label for='name'>Nom :</label>
-    <input type='text' id='name' name='user_name' />
-  </div>
-  <div>
-    <label for='title'>Titre de votre ajout :</label>
-    <input type='text' id='title' name='user_title' />
-  </div>
-  <div>
-    <label for='msg'>Message :</label>
-    <textarea id='msg' name='user_message'>
-      Entrez votre message :
-    </textarea>
-  </div>
-
-  <button type='submit' class='addSubject'>
-    Ajouter un Topic
-  </button>
-</form>; */
-
-// function showHide() {
-//   let statut = document.querySelector('#formIdea');
-//   console.log('TEST');
-//   if (statut.style.display == '') {
-//     statut.style.display = 'none';
-//   } else {
-//     statut.style.display = '';
-//   }
-// }
+// ------------------------FUNCTION AJOUTE DU CONTENU PARTICIPATIF-----------------------------------------
