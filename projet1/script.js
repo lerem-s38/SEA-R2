52593da8c58f083b9a324652775473696882e6c0
Aymeric <aymeric.bouault@gmail.com>
2020-10-01 16:50:26 +0200

    reprise du header

--- projet1/script.js
+++ projet1/script.js
@@ -1,6 +1,25 @@
-// Animation Menu-Burger
-const burger = document.querySelector('.burger');
+ /* const menuderoulant = document.getElementsByClassName('.button-burger');
 
-burger.addEventListener('click', () => {
-    burger.classList.toggle('active');
-})
+menuderoulant.addEventListener('click', function(){
+    const etat = document.getElementsByClassName('sous-menu').style.visibility;
+    if(etat === "hidden"){
+        document.getElementsByClassName('sous-menu').style.visibility="visible";
+    }
+    else{
+        document.getElementsByClassName('sous-menu').style.visibility="Hidden";
+    }
+}, false) */
+
+document.querySelector('.sous-menu').style.display = 'none';
+
+const burgerMenu = document.querySelector('.button-burger');
+burgerMenu.addEventListener('click', burger);
+
+function burger() {
+  let statut = document.querySelector('.sous-menu');
+  if (statut.style.display === 'none') {
+    statut.style.display = 'block';
+  } else {
+    statut.style.display = 'none';
+  }
+}
