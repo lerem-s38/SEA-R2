ed5183fef60777414741e1d212996b237b65d87d
Aymeric <aymeric.bouault@gmail.com>
2020-10-05 11:27:56 +0200

    ajout script page contact

--- projet1/script.js
+++ projet1/script.js
@@ -1,10 +1,45 @@
-/*js menu déroulant*/
-const dropdownBtn = document.querySelector(".btn");
+/* champ requis pour le formulaire*/
 
-const dropdownMenu = document.querySelector(".sous-menu");
+const myForm = document.getElementById('form')
 
+myForm.addEventListener('submit', function(e){
+    const myFirstName = document.getElementById('firstname');
 
-dropdownBtn.addEventListener("click", function() {
-  
-  dropdownMenu.classList.toggle("visible");
-});
+    if (myFirstName.value.trim() === ""){
+        const myError = document.getElementById('error1');
+        myError.innerHTML = "Firstname is required";
+        myError.style.color = "black";
+        e.preventDefault();
+    } 
+
+const myLastName = document.getElementById('lastname');
+
+    if (myLastName.value.trim() === ""){
+        const myError = document.getElementById('error2');
+        myError.innerHTML = "Lastname is required";
+        myError.style.color = "black";
+        e.preventDefault();
+    } 
+
+const myEmail = document.getElementById('exampleFormControlInput1');
+
+    if (myEmail.value.trim() === ""){
+        const myError = document.getElementById('error3');
+        myError.innerHTML = "Email is required";
+        myError.style.color = "black";
+        e.preventDefault();
+    } 
+
+const myMessage = document.getElementById('exampleFormControlTextarea1');
+
+     if(myMessage.value.trim() === ""){
+        const myError = document.getElementById('error4');
+        myError.innerHTML = "Message is required";
+        myError.style.color = "black";
+        e.preventDefault();
+    }else {
+        alert('form send');
+    }
+    
+
+})
